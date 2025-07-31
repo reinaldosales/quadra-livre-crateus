7287b1dcbc1341da8aaec3dd8f5059f9d415f6ab
httpsreinaldo@gmail.com <httpsreinaldo@gmail.com>
2025-07-30 22:31:50 -0300

    feat: endpoint de booking por user

--- src/Backend/QLC/QLC.Api/Services/Abstractions/IBookingService.cs
+++ src/Backend/QLC/QLC.Api/Services/Abstractions/IBookingService.cs
@@ -6,4 +6,5 @@ public interface IBookingService
 {
     public Task CreateBooking(CreateBookingDto bookingDto);
     public Task<IEnumerable<BookingDto>> GetAll();
+    public Task<IEnumerable<BookingDto>> GetAllByUserId(string userId);
 }
